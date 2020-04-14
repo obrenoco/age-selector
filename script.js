1c479a4cf9a903976f910f742913acc33ba77138
breno romeiro <bsromeiro@gmail.com>
2020-04-14 20:20:35 -0300

    Create first version

---
+++ script.js
@@ -0,0 +1,36 @@
+function carregar() {
+    var title = document.getElementById('title')
+    var msg = document.getElementById('text')
+    var img = document.getElementById('img')
+
+    var date = new Date()
+    var horas = date.getHours()
+    var minutos = date.getMinutes()
+
+    var horas = 23
+
+    msg.innerHTML = `Agora são <strong>${horas} horas </strong> e <strong>${minutos} minutos</strong>`
+
+    
+
+    if ( horas >= 0 && horas < 12 ) {
+        // BOM DIA
+        title.innerHTML = `BOM DIA`
+        img.src = 'bomdia.png'
+        document.body.style.background = '#FFF218'
+    } else if (  horas >= 12 && horas <= 18  ) {
+        // BOA TARDE
+        title.innerHTML = `BOA TARDE`
+        img.src = 'boatarde.png'
+        document.body.style.background = '#FF810E'
+    } else if ( horas >= 19 && horas <= 24 ) {
+        //BOA NOITE
+        title.innerHTML = `BOA NOITE`
+        img.src = 'boanoite.png'
+        document.body.style.background = 'black'
+    } else {
+        title.innerHTML = `NOT VALID`
+    }
+
+}
+    
