e922f3724e0c15a815cea9c71b149eb96be56843
breno romeiro <bsromeiro@gmail.com>
2020-04-14 20:28:22 -0300

    organized folders

--- style/script.js
+++ style/script.js
@@ -16,17 +16,17 @@ function carregar() {
     if ( horas >= 0 && horas < 12 ) {
         // BOM DIA
         title.innerHTML = `BOM DIA`
-        img.src = 'bomdia.png'
+        img.src = 'img/bomdia.png'
         document.body.style.background = '#FFF218'
     } else if (  horas >= 12 && horas <= 18  ) {
         // BOA TARDE
         title.innerHTML = `BOA TARDE`
-        img.src = 'boatarde.png'
+        img.src = 'img/boatarde.png'
         document.body.style.background = '#FF810E'
     } else if ( horas >= 19 && horas <= 24 ) {
         //BOA NOITE
         title.innerHTML = `BOA NOITE`
-        img.src = 'boanoite.png'
+        img.src = 'img/boanoite.png'
         document.body.style.background = 'black'
     } else {
         title.innerHTML = `NOT VALID`
